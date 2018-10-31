adb92fd0dc522456aee36f6977cf9f9e217027b4
pythonworkproject <pythonworkproject@gmail.com>
2018-10-31 12:33:49 +0530

    Changes in requirements and migrations

--- work/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from employee_app.models import Employee
-
-
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = ('eid','ename','eemail','econtact','creation_time')
